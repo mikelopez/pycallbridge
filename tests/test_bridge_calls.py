6a2a37851ab69f87c5bd4b3951a7dd91ef95c2a9
Mike Lopez <dev@scidentify.info>
2013-07-29 13:38:54 -0400

    deleted some shit

--- tests/test_bridge_calls.py
+++ tests/test_bridge_calls.py
@@ -11,7 +11,7 @@ sys.path.append('../pycallbridge')
 import settings as s
 
 
-def main(channel = 'sip/kinetic/17863789504', connectTo=('sip/kinetic','17864706212','1')):
+def main(channel = 'sip/kinetic/17863789504', connectTo=('from-internal','17864706212','1')):
 
     f = manager.AMIFactory(getattr(s, "AMI_USER"), getattr(s, "AMI_PASS"))
     df = f.login(getattr(s, "PBX"))
