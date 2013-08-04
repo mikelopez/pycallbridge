e40e456a7326461475f0d395f74dd85f633eae84
Mike Lopez <dev@scidentify.info>
2013-08-04 15:47:36 -0400

    settings import exception

--- pycallbridge/settings.py
+++ pycallbridge/settings.py
@@ -1,1 +1,6 @@
-from local_settings import *
+try:
+    from local_settings import *
+except ImportError:
+    print "No settings found! If you do not want settings, create the\
+    file and leave empty to disregard this message."
+    pass
